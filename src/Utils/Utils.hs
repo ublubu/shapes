30411440fa93b39757b50da6e5978347a625b33a
Kynan Rilee <kynan.rilee@gmail.com>
2015-04-06 18:28:22 -0700

    trying to add stuff for #15 of sdl tutorials

--- src/Utils/Utils.hs
+++ src/Utils/Utils.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Utils.Utils where
 
 import Control.Monad
+import Control.Monad.State hiding (state)
+import Data.Bits
 import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Utils
 import Foreign.Ptr
 import Foreign.Storable
+import GHC.Word
+
+with2 :: (Storable a, Storable b) => a -> b -> (Ptr a -> Ptr b -> IO c) -> IO c
+with2 a b f = with a $ \a' -> with b (f a')
+
 
 withCAString2 :: String -> String -> (CString -> CString -> IO a) -> IO a
 withCAString2 a b op = withCAString a $ \a' -> withCAString b $ op a'
@@ -17,3 +30,17 @@ repeatUntilComplete operation = do
 
 applyToPointer :: (Storable a) => (a -> b) -> Ptr a -> IO b
 applyToPointer operation pointer = liftM operation $ peek pointer
+
+
+infixl 4 ~>>
+(~>>) :: (Monad (t m), Monad m, MonadTrans t) => t m a -> (a -> m b) -> t m b
+(~>>) m f = m >>= lift . f
+
+
+infixl 4 ~>~
+(~>~) :: (Monad (t m), Monad m, MonadTrans t) => t m a -> (a -> m b) -> t m a
+(~>~) m f = m >>= \x -> (lift . f) x >> return x
+
+
+into :: (Monad m, MonadTrans t, MonadState b (t m)) => m a -> (a -> b -> b) -> t m b
+into source f = lift source >>= modify . f >> get
