021808f5e355da52dd605faaf51af45cc8e0075e
Kynan Rilee <kynan.rilee@gmail.com>
2016-03-20 13:54:12 -0700

    dumb quickcheck property

--- shapes-math/test/Shapes/Linear/TemplateSpec.hs
+++ shapes-math/test/Shapes/Linear/TemplateSpec.hs
@@ -4,15 +4,34 @@
 module Shapes.Linear.TemplateSpec where
 
 import Test.Hspec
+import Test.QuickCheck
 
 import GHC.Types (Double(..))
+import Control.Monad
 
 import Shapes.Linear.Template (makeVectorType)
 import Shapes.Linear.ValueInfos (doubleInfo)
+
+import qualified Linear.Metric as L
+import qualified Linear.V2 as L
 
 $(makeVectorType doubleInfo 2)
 
 spec :: Spec
 spec = do
-  it "fromListV2 -> toListV2" $ toListV2 (fromListV2 [1, 2]) `shouldBe` [1, 2]
-  it "dotV2" $ D# (fromListV2 [0, 2] `dotV2` fromListV2 [1, 3]) `shouldBe` 6
+  it "toListV2 fromListV2 == id" $ property $
+    \xy -> let xy' = pairToList xy in toListV2 (fromListV2 xy') == xy'
+  it "dotV2 == L.dot" $ property $
+    \(v1, v2) -> D# (v1 `dotV2` v2) == toLV2 v1 `L.dot` toLV2 v2
+
+pairToList :: (a, a) -> [a]
+pairToList (x, y) = [x, y]
+
+instance Arbitrary V2 where
+  arbitrary = fromListV2 <$> replicateM 2 arbitrary
+
+toLV2 :: V2 -> L.V2 Double
+toLV2 = (\[x, y] -> L.V2 x y) . toListV2
+
+toListLV2 :: L.V2 Double -> [Double]
+toListLV2 (L.V2 x y) = [x, y]
