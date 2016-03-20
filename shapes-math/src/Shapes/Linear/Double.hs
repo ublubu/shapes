717299148940089c8f6336a0e39c04601a9ef70c
Kynan Rilee <kynan.rilee@gmail.com>
2016-03-19 20:47:20 -0700

    Show inst for generated types

--- shapes-math/src/Shapes/Linear/Double.hs
+++ shapes-math/src/Shapes/Linear/Double.hs
@@ -4,10 +4,11 @@
 module Shapes.Linear.Double where
 
 import GHC.Prim
+import GHC.Types (Double(..))
 
-import Shapes.Linear.Template
+import Shapes.Linear.Template (makeVectorType)
 
-$(makeVectorType 2 ''Double#)
+$(makeVectorType 2 ''Double# 'D#)
 
 testV2 :: V2
 testV2 = V2 0.0## 0.0##
--- shapes-math/src/Shapes/Linear/Template.hs
+++ shapes-math/src/Shapes/Linear/Template.hs
@@ -3,10 +3,32 @@
 
 module Shapes.Linear.Template where
 
+import Control.Monad
 import Language.Haskell.TH
 
-makeVectorType :: Int -> Name -> Q [Dec]
-makeVectorType dim valueN = do
+makeVectorType :: Int -> Name -> Name -> Q [Dec]
+makeVectorType dim valueN wrapN = do
   let typeN = mkName $ "V" ++ show dim
       constrArg = (NotStrict, ConT valueN)
-  return [DataD [] typeN [] [NormalC typeN (replicate dim constrArg)] []]
+  showInst <- deriveShow dim typeN wrapN
+  return [ DataD [] typeN [] [NormalC typeN (replicate dim constrArg)] []
+         , showInst
+         ]
+
+data Dummy = Dummy
+
+deriveShow :: Int -> Name -> Name -> Q Dec
+deriveShow dim typeN wrapN = do
+  (pats, vars) <- genPE dim
+  let f [] = [| "" |]
+      f (v:vs) = [| " " ++ show $(AppE (ConE wrapN) <$> v) ++ $(f vs) |]
+      constructorShown = nameBase typeN
+  showClause <- clause [conP typeN pats] (normalB [| constructorShown ++ $(f vars) |]) []
+  [InstanceD [] (AppT showt (ConT _)) [FunD showf _]] <-
+    [d| instance Show Dummy where show _ = "text" |]
+  return $ InstanceD [] (AppT showt (ConT typeN)) [FunD showf [showClause]]
+
+genPE :: Int -> Q ([PatQ], [ExpQ])
+genPE n = do
+  ids <- replicateM n (newName "x")
+  return (map varP ids, map varE ids)
