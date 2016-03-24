1093824bad541df0c9faa097651b5ac66b03ef9e
Kynan Rilee <kynan.rilee@gmail.com>
2016-03-24 15:36:37 -0700

    matrix multiplication test

--- shapes-math/test/Shapes/Linear/TemplateSpec.hs
+++ shapes-math/test/Shapes/Linear/TemplateSpec.hs
@@ -9,12 +9,16 @@ import Test.QuickCheck
 import GHC.Types (Double(..))
 
 import Shapes.Linear.Template (makeVectorType)
+import Shapes.Linear.MatrixTemplate (makeMatrixType, defineMatrixMul)
 import Shapes.Linear.ValueInfos (doubleInfo)
 
 import qualified Linear.Metric as L
 import qualified Linear.V2 as L
+import qualified Linear.Matrix as L
 
 $(makeVectorType doubleInfo 2)
+$(makeMatrixType doubleInfo (2, 2))
+$(defineMatrixMul doubleInfo (2, 2, 2))
 
 spec :: Spec
 spec = do
@@ -23,6 +27,8 @@ spec = do
   it "dotV2 == L.dot" $ property $
     \(v1, v2) -> D# (v1 `dotV2` v2) == toLV2 v1 `L.dot` toLV2 v2
   it "show" $ show (V2 0.0## 1.0##) `shouldBe` "V2 0.0 1.0"
+  it "2x2 * 2x2" $ property $
+    \(m1, m2) -> toListM2x2 (m1 `mul2x2x2` m2) == toListLM22 (toLM22 m1 L.!*! toLM22 m2)
 
 pairToList :: (a, a) -> [a]
 pairToList (x, y) = [x, y]
@@ -30,5 +36,11 @@ pairToList (x, y) = [x, y]
 toLV2 :: V2 -> L.V2 Double
 toLV2 = (\[x, y] -> L.V2 x y) . toListV2
 
+toLM22 :: M2x2 -> L.M22 Double
+toLM22 = (\[a, b, c, d] -> L.V2 (L.V2 a b) (L.V2 c d)) . toListM2x2
+
 toListLV2 :: L.V2 Double -> [Double]
 toListLV2 (L.V2 x y) = [x, y]
+
+toListLM22 :: L.M22 Double -> [Double]
+toListLM22 (L.V2 (L.V2 a b) (L.V2 c d)) = [a, b, c, d]
