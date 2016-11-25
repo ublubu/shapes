d9b6a26244c4869730ca2e0198d14d4c3473e34b
ublubu <kynan.rilee@gmail.com>
2016-11-25 15:20:53 -0500

    meh

--- shapes/src/Physics/Broadphase/AvlTree.hs
+++ shapes/src/Physics/Broadphase/AvlTree.hs
@@ -19,15 +19,36 @@ data Node a = Node { _nodeHeight :: Int
                    , _nodeRight :: Maybe (Node a) }
 makeLenses ''Node
 
-justInsert :: forall a. (a -> Maybe (Node a) -> Maybe (Node a) -> Bool) -> a -> Node a -> Node a
-justInsert chooseFirst val root@Node{..}
-  | chooseFirst val _nodeLeft _nodeRight = insertWith nodeLeft
+type UpdateNodeVal a = a -> Maybe a -> Maybe a -> a
+type ChooseNodeLeft a = a -> Maybe a -> Maybe a -> Bool
+
+justInsert :: forall a. ChooseNodeLeft a
+                     -> UpdateNodeVal a
+                     -> a
+                     -> Node a
+                     -> Node a
+justInsert chooseLeft updateVal val root@Node { .. }
+  | chooseLeftWith chooseLeft root = insertWith nodeLeft
   | otherwise = insertWith nodeRight
-  where setWith branchLens newNode = setHeight (root & branchLens .~ Just newNode)
-        insertWith :: Lens' (Node a) (Maybe (Node a)) -> Node a
-        insertWith branchLens = case root ^. branchLens of
-          Nothing -> setWith branchLens $ newLeaf val
-          Just branchNode -> setWith branchLens $ justInsert chooseFirst val branchNode
+  where
+    setWith branchLens newNode = updateValWith updateVal . setHeight $ (root & branchLens .~ Just newNode)
+    insertWith :: Lens' (Node a) (Maybe (Node a)) -> Node a
+    insertWith branchLens =
+      case root ^. branchLens of
+        Nothing         -> setWith branchLens $ newLeaf val
+        Just branchNode -> setWith branchLens $ justInsert chooseFirst updateVal val branchNode
+
+{- |
+
+ a           b
+  \         / \
+   b   ->  a   c
+    \
+     c
+-}
+leftRotate :: UpdateNodeVal a -> Node a -> Node a
+leftRotate updateVal a@Node { .. } =
+ _
 
 newLeaf :: a -> Node a
 newLeaf val = Node 1 val Nothing Nothing
@@ -35,6 +56,16 @@ newLeaf val = Node 1 val Nothing Nothing
 getHeight :: Maybe (Node a) -> Int
 getHeight Nothing = 0
 getHeight (Just node) = _nodeHeight node
+
+chooseLeftWith :: ChooseNodeLeft a -> Node a -> Bool
+chooseLeftWith chooseLeft Node { .. } = chooseLeft _nodeValue (f _nodeLeft) (f _nodeRight)
+  where f = fmap $ view nodeValue
+
+updateValWith :: UpdateNodeVal a -> Node a -> Node a
+updateValWith updateVal node@Node { .. } =
+  node & nodeValue .~ updateVal _nodeValue (f _nodeLeft) (f _nodeRight)
+  where f = fmap $ view nodeValue
+
 
 setHeight :: Node a -> Node a
 setHeight root@Node { .. } = root & nodeHeight .~ ((max `on` getHeight) _nodeLeft _nodeRight + 1)
