b01cc573f0708714df18908b7ec7e0b1dc75592c
Kynan Rilee <kynan.rilee@gmail.com>
2018-04-29 17:48:45 -0700

    haddocks for Physics.Constraints

--- shapes/src/Physics/Constraints/Contact.hs
+++ shapes/src/Physics/Constraints/Contact.hs
@@ -1,65 +1,81 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 {- |
 Generate and solve all contact constraints for pairs of colliding objects.
 -}
 module Physics.Constraints.Contact where
 
-import GHC.Generics (Generic)
+import           GHC.Generics                               (Generic)
 
-import Control.Lens
-import Control.DeepSeq
-import Data.Vector.Unboxed.Deriving
-import qualified Data.Vector.Unboxed as V
+import           Control.DeepSeq
+import           Control.Lens
+import qualified Data.Vector.Unboxed                        as V
+import           Data.Vector.Unboxed.Deriving
 
-import Physics.Constraint
-import Physics.Constraints.Types
-import Physics.Contact
-import Physics.Contact.ConvexHull
-import qualified Physics.Constraints.Contact.Friction as F
+import           Physics.Constraint
+import qualified Physics.Constraints.Contact.Friction       as F
 import qualified Physics.Constraints.Contact.NonPenetration as NP
-import Utils.Descending
-import Utils.Utils
+import           Physics.Constraints.Types
+import           Physics.Contact
+import           Physics.Contact.ConvexHull
+import           Utils.Descending
+import           Utils.Utils
 
-data ObjectFeatureKey k =
-  ObjectFeatureKey { _ofkObjKeys :: (k, k)
-                   , _ofkFeatKeys :: (Int, Int)
-                   } deriving (Generic, Show, NFData, Eq, Ord)
+{- |
+Indicates a specific pair of "features" on a specific pair of objects that are touching.
+This is how we check if we can reuse a cached solution from the previous frame.
+(We can reuse the cached solution if it has the same 'ObjectFeatureKey')
+-}
+data ObjectFeatureKey k = ObjectFeatureKey
+  { _ofkObjKeys  :: (k, k) -- ^ (first shape's key, second shape's key)
+  , _ofkFeatKeys :: (Int, Int) -- ^ (first shape's feature's key, second shape's feature's key)
+  } deriving (Generic, Show, NFData, Eq, Ord)
 makeLenses ''ObjectFeatureKey
-derivingUnbox "ObjectFeatureKey"
-  [t| forall k. (V.Unbox k) => ObjectFeatureKey k -> ((k, k), (Int, Int)) |]
-  [| \ObjectFeatureKey{..} -> (_ofkObjKeys, _ofkFeatKeys) |]
-  [| uncurry ObjectFeatureKey |]
+derivingUnbox
+  "ObjectFeatureKey"
+  [t|forall k. (V.Unbox k) =>
+                 ObjectFeatureKey k -> ((k, k), (Int, Int))|]
+  [|\ObjectFeatureKey {..} -> (_ofkObjKeys, _ofkFeatKeys)|]
+  [|uncurry ObjectFeatureKey|]
 
-keyedContacts :: (k, k)
-              -> (ConvexHull, ConvexHull)
-              -> Descending (ObjectFeatureKey k, Flipping Contact')
+-- | Calculate all contacts between a pair of shapes.
+keyedContacts ::
+     (k, k)
+  -> (ConvexHull, ConvexHull)
+  -> Descending (ObjectFeatureKey k, Flipping Contact')
 keyedContacts ij ab = fmap f contacts
   where contacts = generateContacts ab
         f (featKeys, contact) = (ObjectFeatureKey ij featKeys, contact)
         {-# INLINE f #-}
 {-# INLINE keyedContacts #-}
 
-constraintGen :: ContactBehavior
-              -> Double
-              -> Flipping Contact'
-              -> (PhysicalObj, PhysicalObj)
-              -> ContactResult Constraint
+-- | Build a constraint from a pair of shapes and a contact between them.
+constraintGen ::
+     ContactBehavior
+  -> Double
+  -> Flipping Contact'
+  -> (PhysicalObj, PhysicalObj)
+  -> ContactResult Constraint
 constraintGen beh dt fContact ab =
   ContactResult { _crNonPen = NP.constraintGen beh dt fContact ab
                 , _crFriction = F.constraintGen fContact ab }
 {-# INLINE constraintGen #-}
 
-solutionProcessor :: (Double, Double)
-                  -> ContactResult Lagrangian
-                  -> ContactResult Lagrangian
-                  -> Processed (ContactResult Lagrangian)
+{- |
+Given an already-applied Lagrangian and the newly-calculated Lagrangian,
+figure out what portion of the newly-calculated Lagrangian should actually be applied.
+-}
+solutionProcessor ::
+     (Double, Double) -- ^ coefficients of friction for a pair of shapes (a, b)
+  -> ContactResult Lagrangian -- ^ cached solution
+  -> ContactResult Lagrangian -- ^ new incremental solution
+  -> Processed (ContactResult Lagrangian) -- ^ 1. incremental solution to actually apply, 2. new cached solution
 solutionProcessor mu_ab (ContactResult npCached fCached) (ContactResult npNew fNew) =
   ContactResult <$> npProcessed <*> fProcessed
   where npProcessed = NP.solutionProcessor npCached npNew
--- shapes/src/Physics/Constraints/SolutionProcessors.hs
+++ shapes/src/Physics/Constraints/SolutionProcessors.hs
@@ -6,36 +6,48 @@ For example, a solution processor might enforce that the total accumulated impul
 -}
 module Physics.Constraints.SolutionProcessors where
 
-import Physics.Constraint
-import Physics.Constraints.Types
+import           Physics.Constraint
+import           Physics.Constraints.Types
 
 wrapProcessor :: (Lagrangian -> Lagrangian -> Lagrangian)
               -> Lagrangian
               -> Lagrangian
               -> Processed Lagrangian
 wrapProcessor f cached_l new_l =
-  Processed { _processedToCache = cached_l'
-            , _processedToApply = apply_l }
-  where cached_l' = cached_l + apply_l
-        apply_l = f cached_l new_l
+  Processed {_processedToCache = cached_l', _processedToApply = apply_l}
+  where
+    cached_l' = cached_l + apply_l
+    apply_l = f cached_l new_l
 {-# INLINE wrapProcessor #-}
 
+-- | Apply the entire newly-calculated Lagrangian.
 simple :: Lagrangian -> Lagrangian -> Processed Lagrangian
 simple = wrapProcessor (flip const)
 {-# INLINE simple #-}
 
+{- |
+Ensure that the sum of the applied Lagrangians is always positive.
+This is useful if a constraint should only apply impulse in one direction.
+e.g. Non-penetration should resist penetration but have no effect on separation.
+-}
 positive :: Lagrangian -> Lagrangian -> Processed Lagrangian
 positive = wrapProcessor (\cached_l new_l -> max new_l (-cached_l))
 {-# INLINE positive #-}
 
+{- |
+Ensure that the magnitude of the sum of the applied Lagrangians never exceeds a threshold.
+This is useful if there's a limit to the force a constraint can apply.
+e.g. Friction resists sliding motion, but this force is limited.
+-}
 clampAbs :: Lagrangian -> Lagrangian -> Lagrangian -> Processed Lagrangian
 clampAbs maxThresh cached new =
-  Processed { _processedToCache = accum_l'
-            , _processedToApply = apply_l }
-  where accum_l = cached + new
-        accum_l' | accum_l > maxThresh = maxThresh
-                 | accum_l < minThresh = minThresh
-                 | otherwise = accum_l
-        apply_l = accum_l' - cached
-        minThresh = -maxThresh
+  Processed {_processedToCache = accum_l', _processedToApply = apply_l}
+  where
+    accum_l = cached + new
+    accum_l'
+      | accum_l > maxThresh = maxThresh
+      | accum_l < minThresh = minThresh
+      | otherwise = accum_l
+    apply_l = accum_l' - cached
+    minThresh = -maxThresh
 {-# INLINE clampAbs #-}
